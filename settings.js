536311953595903f2e5914b3649bdfff461cf911
Tristan7122 <154232254+Tristan7122@users.noreply.github.com>
2025-07-22 18:45:03 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -40,7 +40,7 @@ const settings = {
   
   // 👑 Ownership
   botOwner: "༄❀𝙿𝙷𝙾𝚁𝙰-𝙼𝙳❀༄",
-  ownerNumber: "27634624586",
+  ownerNumber: "27689344972",
   coOwners: [], // Add secondary owners if needed
   
   // ⚙️ Operation Modes
