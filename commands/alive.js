044025eb60ac2eaeb539274e5f13c0e3d6526c92
Tristan7122 <154232254+Tristan7122@users.noreply.github.com>
2025-07-22 17:12:58 +0200

    Update alive.js

--- commands/alive.js
+++ commands/alive.js
@@ -1,7 +1,7 @@
 const settings = require("../settings");
 async function aliveCommand(sock, chatId, message) {
     try {
-        const message1 = `*🤖 Arslan-Ai is Active!*\n\n` +
+        const message1 = `*🤖 ༄❀𝙿𝙷𝙾𝚁𝙰-𝙼𝙳❀༄ is Active!*\n\n` +
                        `*Version:* ${settings.version}\n` +
                        `*Status:* Online\n` +
                        `*Mode:* Public\n\n` +
